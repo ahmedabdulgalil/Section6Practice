4aa2e4132a4c42bbc1f7d3444d4111e78e802788
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-07-22 08:34:18 -0400

    updated

---
+++ Section6Practice/src/inheritance/Main.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Animal animal = new Animal ("Animal", 1,1, 5,5);
+		
+		Dog dog = new Dog ("Yorkie", 8, 20, 2, 4 ,1, 20, " Long silky");
+		//dog.eat();
+		dog.walk();
+		//dog.run();
+
+	}
+
+}
